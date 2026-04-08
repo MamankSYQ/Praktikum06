4ac83ae49ad8c9c9dd8021470df922c537e8b6f1
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2026-04-08 08:10:28 +0700

    Sequential Search

---
+++ MahasiswaBerprestasi01.java
@@ -0,0 +1,49 @@
+public class MahasiswaBerprestasi01 {
+    Mahasiswa01[] listMhs = new Mahasiswa01[5];
+    int idx;
+
+    void tambah (Mahasiswa01 m){
+        if (idx < listMhs.length){
+            listMhs[idx] = m;
+            idx++;
+        }
+        else{
+            System.out.println("Data sudah penuh");
+        }
+    }
+    void tampil(){
+        for (int i = 0; i < idx; i++){
+            listMhs[i].tampilInformasi();
+            System.out.println("------------------------------");
+        }
+    }
+    int sequentialSearching(double cari){
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++){
+            if (listMhs[j].ipk == cari){
+                posisi= j;
+                break;
+            }
+        }
+        return posisi;
+    }
+    void tampilPosisi(double x, int pos){
+        if (pos != -1){
+            System.out.println("Data mahasiswa dengan IPK : "+x+" ditemukan pada index "+pos);
+        }
+        else{
+            System.out.println("Data "+x+" tidak ditemukan");
+        }
+    }
+    void tampilDataSearch(double x, int pos){
+        if (pos != -1){
+            System.out.println("NIM\t : "+listMhs[pos].nim);
+            System.out.println("Nama\t : "+listMhs[pos].nama);
+            System.out.println("Kelas\t : "+listMhs[pos].kelas);
+            System.out.println("IPK\t : "+x);
+        }
+        else{
+            System.out.println("Data mahasiswa dengan IPK "+x+" tidak ditemukan");
+        }
+    }
+}
